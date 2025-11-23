b990e255c666861b9390aa32b1963ceabde5100a
Nicole Alcala <nicolealcalq@gmail.com>
2025-11-23 21:50:10 +0800

    Add client-side auth validator (draft) in RootLayout component

--- client/src/components/layout/RootLayout.jsx
+++ client/src/components/layout/RootLayout.jsx
@@ -1,10 +1,35 @@
-// layouts/RootLayout.jsx
-import { SidebarProvider, SidebarTrigger } from "../ui/sidebar.jsx";
-import { Outlet, NavLink } from "react-router-dom";
+import React from "react";
+import { SidebarProvider } from "../ui/sidebar.jsx";
+import { Navigate, Outlet } from "react-router-dom";
 import { AppSidebar } from "./Sidebar.jsx";
 import Header from "./Header.jsx";
+import { supabase } from "@/lib/config/supabaseClient.js";
+import Loader from "../customized/Loader.jsx";
+import { Toaster } from "@/components/ui/sonner";
 
 export default function RootLayout() {
+  const [loading, setLoading] = React.useState(true);
+  const [session, setSession] = React.useState(null);
+
+  React.useEffect(() => {
+    async function fetchSession() {
+      try {
+        const { data } = await supabase.auth.getSession();
+        setSession(data.session);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching session:", error);
+        setSession(null);
+        setLoading(false);
+      }
+    }
+
+    fetchSession();
+  });
+
+  if (loading) return <Loader />;
+  if (!session) return <Navigate to="/auth" replace />;
+
   return (
     <SidebarProvider className="flex w-full">
       <AppSidebar />
@@ -13,6 +38,7 @@ export default function RootLayout() {
         <main className="max-w-7xl w-full h-full p-6 pl-5 py-6 z-0">
           <Outlet />
         </main>
+        <Toaster />
       </div>
     </SidebarProvider>
   );
