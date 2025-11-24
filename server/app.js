d6e4f6e19a29f260c47d0f37960a3f54d4d28551
Nicole Alcala <nicolealcalq@gmail.com>
2025-11-24 15:55:51 +0800

    Add auth router and cors middleware

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import userRouter from './routes/userRoutes.js';
 import applicationRouter from './routes/applicationRoutes.js';
 import scheduleRouter from './routes/scheduleRoutes.js';
 import employmentRecordRouter from './routes/employmentRecordRoutes.js';
+import authRouter from './routes/authRoutes.js';
 
 const app = express();
 
@@ -11,6 +13,15 @@ export const CONTENT_STATUS = {
     empty: "EMPTY"
 }
 
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+
+app.use(express.json());
+
+app.use('/api/auth', authRouter)
 app.use('/api/users', userRouter);
 app.use('/api/employment-records', employmentRecordRouter);
 app.use('/api/applications', applicationRouter);
